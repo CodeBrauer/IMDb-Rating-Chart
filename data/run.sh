6a561e27ab99070d87d0447be1ef384662164230
CodeBrauer <codebrauer@gmail.com>
2018-12-10 00:31:24 +0100

    Add download script

---
+++ data/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://www.imdb.com/conditions
+
+echo -ne 'Downloading files...\n'
+
+wget https://datasets.imdbws.com/title.akas.tsv.gz
+wget https://datasets.imdbws.com/title.episode.tsv.gz
+wget https://datasets.imdbws.com/title.ratings.tsv.gz
+
+echo -ne 'Decompress files...\n'
+
+gzip -d title.akas.tsv.gz
+gzip -d title.episode.tsv.gz
+gzip -d title.ratings.tsv.gz
+
+echo -ne 'Done!\n'
